e03f944e3665aa952311c500edc21e2abe805879
nagamatsu-k <nagamatsu-k@dym.jp>
2022-08-24 09:12:39 +0900

    READMEの作成とconfigサンプルの作成

---
+++ README.md
@@ -0,0 +1,5 @@
+## git clone後の初期設定
+```shell
+$ cd nextjs
+$ cp -R config_sample config
+```
---
+++ nextjs/config_sample/endpoint.ts
@@ -0,0 +1,5 @@
+const Endpoint = {
+  api: "http://localhost:8000/api/v1/",
+  front: "http://localhost:3000/",
+}
+export default Endpoint
